f8f35f248c60c71f1fb7eb4a15161022cecfa07c
Ashe <molly.02.andrews@gmail.com>
2022-08-10 22:03:04 -0400

    Print board and player choose turn for tic tac toe

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -17,13 +17,46 @@ int isFilled(char positionValue) {
 }
 
 void printBoard(char board[3][3]) {
+    printf("Current board:\n");
+    
+    for (int r = 0; r < 3; r++) {
+        char row[3] = {};
+        
+        for (int c = 0; c < 3; c++) {
+            row[c] = board[r][c];
+        }
+
+        printf("%c %c %c\n", row[0], row[1], row[2]);
+    }
+    
     return;
 }
 
-void main() {
-    printf("Welcome to tic-tac-toe!");
-    char board[3][3] = {{'0', '0', '0'}, {'0', '0', '0'}, {'0', '0', '0'}}; // rows x columns
-    return;
+int main() {
+    printf("Welcome to tic-tac-toe!\n");
+    char board[3][3] = {{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}}; // rows x columns
+    printBoard(board);
+
+    printf("Enter X to go first or O to go second.\n");
+    char playerChar = getchar();
+    printf("You picked %c", playerChar);
+    
+    char computerChar;
+    int playerTurn;
+    if (playerChar == 'X' || playerChar == 'x') {
+        computerChar = 'O';
+        playerTurn = 1;
+    }
+    else if (playerChar == 'O' || playerChar == 'o'){
+        computerChar = 'X';
+        playerTurn = 0;
+    }
+    else {
+        printf("Invalid entry. Enter X or O.");
+        return 1;
+    }
+    
+    return 0;
 }
 
-//TODO: Print board, player select X (first) or O (second), player turn, computer turn, check if move valid, check if game over
+//TODO: player select X (first) or O (second), player turn, computer turn, check if move valid, check if game over
